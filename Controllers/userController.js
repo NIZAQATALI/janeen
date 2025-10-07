cf6ca2bdeaf5e401ba20683ae1a5d60e66c314a7
Nizaqat Ali <hafiznizaqatali@gmail.com>
2025-10-08 00:34:56 +0500

    update login response with role equel gender

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -111,7 +111,7 @@ export const loginUser = async (req, res) => {
           id: user._id,
           username: user.username,
           email: user.email,
-          role: user.role,
+          role: user.gender.toLowerCase(),
         },
       },
     });
