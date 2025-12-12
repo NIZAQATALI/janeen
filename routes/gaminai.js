9d17000e35e3422edc3aea50765a97f29125bcca
Nizaqat Ali <hafiznizaqatali@gmail.com>
2025-12-13 03:53:04 +0500

    folder name mismatch issue

--- routes/gaminai.js
+++ routes/gaminai.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { chatWithGemini } from "../controllers/gaminaiController.js";
+import { chatWithGemini } from "../Controllers/gaminaiController.js";
 import { verifyAdmin ,verifyJWT} from "../utils/verifyToken.js";
 
 const router = express.Router();
